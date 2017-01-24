666b6f5e880741190cfc85cc5daafdc8907244bc
Xiao Liu <liuxiao232@gmail.com>
2017-01-24 09:38:28 -0600

    Create

--- src/Arrays_Strings/Q1_Is_Unique.java
+++ src/Arrays_Strings/Q1_Is_Unique.java
@@ -5,4 +5,25 @@ package Arrays_Strings;
  */
 
 public class Q1_Is_Unique {
+    private static final String testStrUnique = "abcde ipgk";
+    private static final String testStr = "Hello World";
+
+    public static void main(String[] args){
+        System.out.println(isUnique(testStrUnique));
+        System.out.println(isUnique(testStr));
+    }
+
+    private static boolean isUnique(String str){
+        int[] charMap = new int[128];
+
+        for (int i=0; i<str.length(); i++){
+            if (charMap[str.charAt(i)] == 0){
+                charMap[str.charAt(i)]++;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
 }
